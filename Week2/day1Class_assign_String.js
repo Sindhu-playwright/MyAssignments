3c3f782c2166ceb66b8360dc62070fa1339d956f
Sindhu-playwright <sindhujudison@gmail.com>
2026-05-27 12:55:11 +0530

    Add files via upload

---
+++ Week2/day1Class_assign_String.js
@@ -0,0 +1,13 @@
+//expec -> testleaf to lowercase and uppercase
+//let c="Playwright automation project"  c.split("a")
+
+let a = "TestLeaf"
+console.log(a.toLowerCase())
+console.log(a.toUpperCase())
+
+
+let c="Playwright automation project"
+console.log(c.split("a"))
+for(let i=0; i<c.length; i++){
+    console.log(c[i])
+}
